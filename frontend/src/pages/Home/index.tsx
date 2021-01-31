2cdc1278a73e949e824905c23a46b5e3becd4fb5
ngustavin <gust4f3rn@gmail.com>
2021-01-31 18:57:23 -0300

    Home page created

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,229 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, TodoList, Todo, TopBarContainer } from './styles';
+import background from '../../assets/home/background.svg'
+import logo from '../../assets/home/logo.svg'
+import check from '../../assets/home/check.svg'
+
+import Checkbox from '@material-ui/core/Checkbox'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+
+
 
 const Home: React.FC = () => {
-  return <div />;
+
+
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  return (
+      <Container>
+          <img src={background} alt=""/>
+          <TopBarContainer>
+            <img src={logo} alt=""/>
+            <img src={check} alt=""/>
+            <button type="submit">+ Add</button>
+          </TopBarContainer>
+          <TodoList>
+            <Todo>
+              <span>Walk the dog</span>
+              <div>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                style={{color: "#D95B5B"}}
+              />
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <DeleteIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <EditIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              </div>
+            </Todo>
+
+            <Todo>
+              <span>Walk the dog</span>
+              <div>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                style={{color: "#D95B5B"}}
+              />
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <DeleteIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <EditIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              </div>
+            </Todo>
+
+            <Todo>
+              <span>Walk the dog</span>
+              <div>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                style={{color: "#D95B5B"}}
+              />
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <DeleteIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <EditIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              </div>
+            </Todo>
+            <Todo>
+              <span>Walk the dog</span>
+              <div>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                style={{color: "#D95B5B"}}
+              />
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <DeleteIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <EditIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              </div>
+            </Todo>
+            <Todo>
+              <span>Walk the dog</span>
+              <div>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                style={{color: "#D95B5B"}}
+              />
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <DeleteIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <EditIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              </div>
+            </Todo>
+            <Todo>
+              <span>Walk the dog</span>
+              <div>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                style={{color: "#D95B5B"}}
+              />
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <DeleteIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <EditIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              </div>
+            </Todo>
+            <Todo>
+              <span>Walk the dog</span>
+              <div>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                style={{color: "#D95B5B"}}
+              />
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <DeleteIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <EditIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              </div>
+            </Todo>
+            <Todo>
+              <span>Walk the dog</span>
+              <div>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                style={{color: "#D95B5B"}}
+              />
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <DeleteIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <EditIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              </div>
+            </Todo>
+            <Todo>
+              <span>Walk the dog</span>
+              <div>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                style={{color: "#D95B5B"}}
+              />
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <DeleteIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <EditIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              </div>
+            </Todo>
+            <Todo>
+              <span>Walk the dog</span>
+              <div>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                style={{color: "#D95B5B"}}
+              />
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <DeleteIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <EditIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              </div>
+            </Todo>
+            <Todo>
+              <span>Walk the dog</span>
+              <div>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                style={{color: "#D95B5B"}}
+              />
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <DeleteIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              <IconButton aria-label="delete" style={{color: "#D95B5B"}}>
+               <EditIcon style={{color: "#D95B5B"}} />
+              </IconButton>
+              </div>
+            </Todo>
+            
+          </TodoList>
+      </Container>
+  );
 }
 
 export default Home;
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,5 +1,115 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    background: #F0F0F2;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    overflow-y: hidden;
     
+    > img{
+        width: 100vw;
+        height: auto;
+    }
+`
+export const TopBarContainer = styled.div`
+    position: absolute;
+    width: 100vw;
+    height: 200px;
+    top: 0;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    padding: 46px 72px 0px 72px;
+
+    > img:first-child{
+        width: 95.75px;
+        height: auto;
+        max-width: 100%;
+
+        :hover{
+            cursor: pointer;
+        }
+    }
+
+    > button{
+        width: 160px;
+        height: 58px;
+        border-radius: 54px;
+        background: none;
+        border: 3px solid #D95B5B;
+        color: #D95B5B;
+        font-size: 30px;
+        transition: 0.3s;
+        :hover{
+            cursor: pointer;
+            background: #D95B5B;
+            color: white;
+            border-radius: 4px;
+        }
+    }
+`
+
+export const TodoList = styled.div`
+    position: absolute;
+    left: 28%;
+    background: none;
+    top: 300px;
+    width: 798px;
+    height: 685px;
+    overflow-y: scroll;
+    display: grid;
+    grid-auto-rows: 60px;
+    grid-gap: 30px;
+    overflow-x: hidden;
+    padding: 4px 5px 0px 5px;
+
+    /* width */
+    ::-webkit-scrollbar {
+        width: 6px;
+        border-radius: 5px;
+        background: none;
+    }
+
+    /* Track */
+    /* ::-webkit-scrollbar-track {
+        width: 10px;
+        background: #FFF;
+        border-radius: 5px;
+    } */
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        width: 10px;
+        background: #FFF;
+        border-radius: 5px;
+    }
+
+   
+
+`
+
+export const Todo = styled.div`
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    background: #FFF;
+    margin-right: 40px;
+    transition: 0.2s;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 0px 0px 15px;
+
+    >span {
+        color: #000000;
+        font-size: 18px;
+        font-family: 'Ubuntu', monospace;
+        font-weight: 400;
+    }
+    :hover{
+        box-shadow: 0px 0px 0px 3px #D95B5B;
+    }
 `
