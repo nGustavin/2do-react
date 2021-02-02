8a4b97e3015d792098b34c20c4717135a53ea85d
ngustavin <gust4f3rn@gmail.com>
2021-02-01 21:31:56 -0300

    backend: list todos controller created

---
+++ backend/src/controllers/todo/index.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import {ITodo} from './../../types/todo'
+import Todo from '../../models/todo'
+
+const getTodos = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const todos: ITodo[] = await Todo.find()
+        res.status(200).json({ todos })
+    } catch (error) {
+        throw error
+    }
+}
+
+const addTodo = async (req: Request, res: Response): Promise<void> => {
+    
+}
