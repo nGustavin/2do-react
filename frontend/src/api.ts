acdcadbab5b08356fe6d903a443cb84e7d59cda7
ngustavin <gust4f3rn@gmail.com>
2021-02-01 22:25:07 -0300

    frontend: axios getTodo request added

---
+++ frontend/src/api.ts
@@ -0,0 +1,12 @@
+import axios, { AxiosResponse } from 'axios'
+
+const baseUrl: string = "http://localhost:3333"
+
+export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
+    try {
+        const todos: AxiosResponse<ApiDataType> = await axios.get(baseUrl + "/todos")
+        return(todos)
+    } catch (error) {
+        throw new Error(error)
+    }
+}
