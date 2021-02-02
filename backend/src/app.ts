d1c330bbe9ad45d91e3e67f87e59d626a9707990
ngustavin <gust4f3rn@gmail.com>
2021-02-01 22:18:56 -0300

    backend: app.ts created with MONGO_DB connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
 import express, {Express} from 'express'
-import mongoose from 'mongoose'
+import mongoose, { mongo } from 'mongoose'
 import cors from 'cors'
 import todoRoutes from './routes'
 
@@ -8,3 +8,19 @@ const app: Express = express()
 const PORT: string | number = process.env.PORT || 3333
 
 app.use(cors())
+app.use(todoRoutes)
+
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@todoapp.wibjk.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.set("useFindAndModify", false)
+
+mongoose
+    .connect(uri, options)
+    .then(() => 
+        app.listen(PORT, () =>
+            console.log(`Server listening on the http://localhost:${PORT}`)
+        )
+    )
+    .catch(error => {
+        throw error
+    })
