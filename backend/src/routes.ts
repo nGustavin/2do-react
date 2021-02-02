8753f0a86a576dca9f7d953004ea0e16182ffe0c
ngustavin <gust4f3rn@gmail.com>
2021-02-01 21:50:49 -0300

    backend: add todo route created

---
+++ backend/src/routes.ts
@@ -0,0 +1,14 @@
+import {Router} from 'express'
+import { addTodo, deleteTodo, updateTodo, getTodos } from './controllers/todo/index'
+
+const router: Router = Router()
+
+router.get("/todos", getTodos)
+
+router.post("/add-todo", addTodo)
+
+router.put("/edit-todo/:id", updateTodo)
+
+router.delete("/delete-todo/:id", deleteTodo)
+
+export default router
