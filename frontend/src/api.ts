e083529193a65b56f48fbfa8a7a53f3e62299178
ngustavin <gust4f3rn@gmail.com>
2021-02-01 22:28:42 -0300

    frontend: axios addTodo request added

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios'
+import axios, { AxiosResponse, AxiosResponse } from 'axios'
 
 const baseUrl: string = "http://localhost:3333"
 
@@ -10,3 +10,24 @@ export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
         throw new Error(error)
     }
 }
+
+export const addTodo = async (
+    formData: ITodo
+): Promise<AxiosResponse<ApiDataType>> => {
+    try {
+        const todo: Omit<ITodo, "_id"> = {
+            name: formData.name,
+            description: formData.description,
+            status: false,
+        }
+
+        const saveTodo: AxiosResponse<ApiDataType> = await axios.post(
+            baseUrl + "/add-todo", 
+            todo
+        )
+        return(saveTodo)
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
